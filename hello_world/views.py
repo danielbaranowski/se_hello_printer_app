4107d4448d5d2c533f05dc8a82fdb3fb7d2b6cae
danielbaranowski <danielbaranowski@users.noreply.github.com>
2022-05-28 14:33:02 +0200

    file views.py added

---
+++ hello_world/views.py
@@ -0,0 +1,20 @@
+from hello_world import app
+from hello_world.formatter import get_formatted
+from hello_world.formatter import SUPPORTED, PLAIN
+from flask import request
+
+moje_imie = "Daniel"
+msg = "Hello World!"
+
+@app.route('/')
+def index():
+    output = request.args.get('output')
+    if not output:
+        output = PLAIN
+       return get_formatted(msg, moje_imie,
+                         output.lower())
+
+@app.route('/outputs')
+def supported_output():
+    return ", ".join(SUPPORTED)
+    

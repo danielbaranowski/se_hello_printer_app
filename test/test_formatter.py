9510b800598a265c67f74c6d01bbe0bc8c6b83c5
danielbaranowski <danielbaranowski@users.noreply.github.com>
2022-05-29 14:09:12 +0200

    added several files

---
+++ test/test_formatter.py
@@ -0,0 +1,11 @@
+from hello_world.formatter import plain_text_upper_case
+import unittest
+
+
+class TestFormatter(unittest.TestCase):
+    def test_plain_uppercase(self):
+        r = plain_text_upper_case("wwww", "EEEMSG")
+        name = r.split(" ")[0]
+        msg = r.split(" ")[1]
+        self.assertTrue(name.isupper())
+        self.assertTrue(msg.isupper())
